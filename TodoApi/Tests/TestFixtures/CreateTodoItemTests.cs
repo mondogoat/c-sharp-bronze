82fd3f4b39e2e1cd82228e029faa2a6b5a015328
Carlo Candoy <carlo.candoy@3cloudsolutions.com>
2024-02-23 19:44:04 +0800

    Add initial test to CreateTodoItem

---
+++ TodoApi/Tests/TestFixtures/CreateTodoItemTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using RestSharp;
+using FluentAssertions;
+using Tests.Models;
+
+namespace Tests.TestFixtures;
+
+public class CreateTodoItemTests
+{
+    [Test]
+    public void CreateTodoItemSuccess()
+    {
+        var endpoint = "http://localhost:8080/api/TodoItems";
+
+        var client = new RestClient(endpoint);
+        var request = new RestRequest();
+        request.Method = Method.Post;
+
+        var payload = new TodoItemModel
+        {
+            name = "test3",
+            isComplete = false
+        };
+        request.AddJsonBody(payload);
+        
+        var response = client.Execute(request);
+        var responseBody = JsonConvert.DeserializeObject<TodoItemModel>(response.Content);
+
+        Console.WriteLine(response.Content);
+        
+        // Validate status code
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        responseBody.Should().NotBeNull();
+        responseBody.id.Should().NotBe(null);
+    }
+}
