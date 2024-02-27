89f207dab4394b2e87b10aa8afb368d161954785
Carlo Candoy <carlo.candoy@3cloudsolutions.com>
2024-02-28 01:00:53 +0800

    Added setup method for GetAllTodo Tests

--- TodoApi/Tests/TestFixtures/GetAllTodoTests.cs
+++ TodoApi/Tests/TestFixtures/GetAllTodoTests.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using RestSharp;
 using FluentAssertions;
+using NUnit.Framework.Internal;
 using Tests.Models;
 
 namespace Tests.TestFixtures;
@@ -15,19 +16,41 @@ public class GetAllTodoTests
         _testUtilities = new TestUtilities("http://localhost:8080/api");
     }
 
+    [OneTimeSetUp]
+    public void PopulateTodoList()
+    {
+        var endpoint = "TodoItems";
+        
+        for (int i = 1; i <= 3; i++)
+        {
+            var payload = new TodoItemModel
+            {
+                name = $"todo item {i}",
+                isComplete = false
+            };
+            
+            _testUtilities.ExecutePostRequest<TodoItemModel>(endpoint, payload);
+        }
+    }
+
     [Test]
     public void GetAllTodoSuccess()
     {
+        // TODO: Create 3 TodoItems first then execute GET request to fetch list of TODO
         // arrange
         var endpoint = "TodoItems";
-        // var payload = new TodoItemModel()
+        // var payload = new TodoItemModel
         // {
-        //     
-        // }
-        // _testUtilities.ExecutePostRequest<TodoItemModel>(payload);
+        //     name = "test3",
+        //     isComplete = false
+        // };
+        // _testUtilities.ExecutePostRequest<TodoItemModel>("TodoItems", payload);
+        // _testUtilities.ExecutePostRequest<TodoItemModel>("TodoItems", payload);
+        // _testUtilities.ExecutePostRequest<TodoItemModel>("TodoItems", payload);
         
         // act
         var (responseBody, statusCode) = _testUtilities.ExecuteGetRequest<List<TodoItemModel>>(endpoint);
+        Console.WriteLine(JsonConvert.SerializeObject(responseBody));
         
         // assert
         statusCode.Should().Be(HttpStatusCode.OK);
