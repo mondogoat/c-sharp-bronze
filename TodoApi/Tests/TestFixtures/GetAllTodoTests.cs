834c5fa171d75cd64ce58f4bcdff5dde85355cbd
Carlo Candoy <carlo.candoy@3cloudsolutions.com>
2024-02-28 21:19:28 +0800

    Added setup and teardown for Get All tests

--- TodoApi/Tests/TestFixtures/GetAllTodoTests.cs
+++ TodoApi/Tests/TestFixtures/GetAllTodoTests.cs
@@ -7,21 +7,20 @@ using Tests.Models;
 
 namespace Tests.TestFixtures;
 
+[TestFixture]
 public class GetAllTodoTests
 {
     private readonly TestUtilities _testUtilities;
-
+    private readonly string _endpoint = "TodoItems";
     public GetAllTodoTests()
     {
         _testUtilities = new TestUtilities("http://localhost:8080/api");
     }
-
-    [OneTimeSetUp]
+    
+    [SetUp]
     public void PopulateTodoList()
     {
-        var endpoint = "TodoItems";
-        
-        for (int i = 1; i <= 3; i++)
+        for (int i = 1; i <= 5; i++)
         {
             var payload = new TodoItemModel
             {
@@ -29,31 +28,65 @@ public class GetAllTodoTests
                 isComplete = false
             };
             
-            _testUtilities.ExecutePostRequest<TodoItemModel>(endpoint, payload);
+            _testUtilities.ExecutePostRequest<TodoItemModel>(_endpoint, payload);
         }
     }
 
-    [Test]
-    public void GetAllTodoSuccess()
+    [TearDown]
+    public void ClearTodoList()
     {
-        // TODO: Create 3 TodoItems first then execute GET request to fetch list of TODO
+        var (todoItems, _) = _testUtilities.ExecuteGetRequest<List<TodoItemModel>>(_endpoint);
+
+        if (todoItems != null && todoItems.Any())
+        {
+            foreach (var todoItem in todoItems)
+            {
+                _testUtilities.ExecuteDeleteRequest(_endpoint, todoItem.id);
+            }
+        }
+    }
+    
+    [Test]
+    public void GetAllTodo_Success()
+    {
+        // TODO: Create 5 TodoItems first then execute GET request to fetch list of TODO
         // arrange
-        var endpoint = "TodoItems";
-        // var payload = new TodoItemModel
-        // {
-        //     name = "test3",
-        //     isComplete = false
-        // };
-        // _testUtilities.ExecutePostRequest<TodoItemModel>("TodoItems", payload);
-        // _testUtilities.ExecutePostRequest<TodoItemModel>("TodoItems", payload);
-        // _testUtilities.ExecutePostRequest<TodoItemModel>("TodoItems", payload);
         
         // act
-        var (responseBody, statusCode) = _testUtilities.ExecuteGetRequest<List<TodoItemModel>>(endpoint);
+        var (responseBody, statusCode) = _testUtilities.ExecuteGetRequest<List<TodoItemModel>>(_endpoint);
         Console.WriteLine(JsonConvert.SerializeObject(responseBody));
         
         // assert
         statusCode.Should().Be(HttpStatusCode.OK);
         responseBody.Should().NotBeNullOrEmpty();
+    }
+
+    [Test]
+    public void GetAllTodo_IsCompleteValueFilterTrue()
+    {
+    }
+    
+    [Test]
+    public void GetAllTodo_IsCompleteValueFilterFalse()
+    {
+    }
+
+    [Test]
+    public void GetAllTodo_SuccessWithEmptyListIfNoTodoItem()
+    {
+        // arrange
+        ClearTodoList();
+        
+        // act
+        var (responseBody, statusCode) = _testUtilities.ExecuteGetRequest<List<TodoItemModel>>(_endpoint);
+        
+        // assert
+        statusCode.Should().Be(HttpStatusCode.OK);
+        responseBody.Should().BeEmpty();
+    }
+
+    [Test]
+    public void GetAllTodo_Pagination()
+    {
     }
 }
