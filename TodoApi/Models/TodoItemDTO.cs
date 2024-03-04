d6a744773c4b1e5c549fc8b1e2c017a897ca5996
Carlo Candoy <carlo.candoy@3cloudsolutions.com>
2024-03-04 23:36:30 +0800

    Added CreatedTime and CompletedTime in TodoItem Model. Modified POST and PUT endpoints to update CreatedTime and Completed Time upon api call.

--- TodoApi/Models/TodoItemDTO.cs
+++ TodoApi/Models/TodoItemDTO.cs
@@ -5,4 +5,6 @@ public class TodoItemDTO
     public long Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+    public DateTime CreatedTime { get; set; }
+    public DateTime? CompletedTime { get; set; }
 }
